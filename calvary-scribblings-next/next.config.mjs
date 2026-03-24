51befcc20e7c0da4dc14cc49928c51b1461496f0
calvaryscribblings <calvarymediauk@gmail.com>
2026-03-24 17:00:50 +0000

    Add output export for Cloudflare deployment

--- calvary-scribblings-next/next.config.mjs
+++ calvary-scribblings-next/next.config.mjs
@@ -4,6 +4,7 @@ const withMDX = createMDX({});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'ts', 'tsx', 'md', 'mdx'],
   images: {
     unoptimized: true,
